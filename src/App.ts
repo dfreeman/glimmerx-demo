169ff9c6865c801fc1acffb1e9b5c29b85d78522
Dan Freeman <dfreeman@salsify.com>
2020-06-26 18:44:16 +0200

    Just playing around

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,23 @@
-import Component, { hbs } from '@glimmerx/component';
+/* eslint-disable */
+import Component, { hbs } from "@glimmerx/component";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+type GreetingArgs = {
+  message: string;
+  target?: string;
+}
+
+class Greeting extends Component<GreetingArgs> {
+  private get target() {
+    return this.args.target ?? 'glimmerx';
+  }
+
+  public static template = hbs`
+    {{@message}}, {{this.target}}!
+  `;
+}
 
 export default class App extends Component {
   private logo = logo;
@@ -10,7 +26,9 @@ export default class App extends Component {
     <div id="intro">
       <img src={{this.logo}}/>
 
-      <h1>hello, glimmerx!</h1>
+      <h1>
+        <Greeting @message="hello" @target="glint" />
+      </h1>
       <h3>
         you can get started by editing <code>src/App.js</code>,
         and run tests by visiting <a href="./tests">/tests</a>
